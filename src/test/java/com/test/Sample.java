1ee743e96c8043be6b158ff2a7c67399c5ce3824
omrsolairaj-cloud~ <omrsolairaj@gmail.com>
2025-10-29 13:33:13 +0530

    Calculator Master

---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Sample {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum: " + add(5, 3));
+        System.out.println("Difference: " + subtract(5, 3));
+    }
+}
+
